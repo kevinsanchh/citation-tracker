a688cf0b209f9d256c71d19cc286e9008428e7fd
kevinsanchh <kevinsanchez213@yahoo.com>
2025-09-25 22:47:54 -0400

    vercel build error fix 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,7 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next',"next/core-web-vitals", "next/typescript"],
     rules: {
-      ' @typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ];
