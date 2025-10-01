1ea8264769c4ec9a04cf0ce9e3a1221c2f5a2f02
kevinsanchh <kevinsanchez213@yahoo.com>
2025-10-01 13:12:48 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import AnimatedTabs from "@/components/animated-tabs";
 
 export default function Home() {
   return (
-    <main className="h-full w-full bg-yellow-400 relative">
+    <main className="h-full w-full bg-white relative">
       <div className="w-80 bg-white/80 backdrop-blur-xl h-full border-r border-gray-200/50 flex flex-col absolute z-20">
         <div className="p-4 flex justify-center items-center border-b border-gray-200/50">
           <h1 className="font-semibold text-xl text-neutral-900">FIU Citation Tracker</h1>
@@ -11,7 +11,7 @@ export default function Home() {
         <AnimatedTabs />
       </div>
       {/*the map */}
-      <MapComponent />
+      {/* <MapComponent /> */}
     </main>
   );
 }
